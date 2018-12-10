71d1dd930ddbc219cc98e690c478156bd4894b45
gupta <39546074+guptagrandhi@users.noreply.github.com>
2018-12-10 09:56:14 +0530

    Add files via upload

---
+++ nine.js
@@ -0,0 +1,17 @@
+
+//validaton for form
+
+
+function  ValidateForm()
+var x=document.form1.fname.value;
+{
+if(x == "" )
+         {
+           alert("First Name is required");
+           return false;
+		}
+}
+
+
+
+	
